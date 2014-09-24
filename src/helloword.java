fb10c318cf237e8a91e0b02273d38f44a006895f
Dekey <Dky123@mail.ru>
2014-09-24 21:31:38 +0400

    Initial commit

---
+++ src/helloword.java
@@ -0,0 +1,14 @@
+public class helloword {
+    public static void main(String[] args) {
+      String  a ="Petr";
+        String b = "Ivan";
+        String c = "Privet";
+        String Priv = c+" "+a;
+        String Privet = c+ " " +b;
+        System.out.println(Priv);
+        System.out.println(Privet);
+
+
+
+    }
+}
